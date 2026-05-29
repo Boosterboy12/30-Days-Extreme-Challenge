df6e852214ac44ff35b48eab31698ca9bcafed45
Boosterboy12 <Boosterboy2030@gmail.com>
2026-05-29 11:51:36 +0530

    Day-4: Built An Adavnce Pandas Pipeline

--- Day-1.py
+++ Day-1.py
@@ -1,80 +1,80 @@
-#Importing Numpy 
-import numpy as np 
+# Importing Numpy
+import numpy as np
 
-#Level-1
+# Level-1
 
-#Task-1 
-array1 = np.arange(10,50)
+# Task-1
+array1 = np.arange(10, 50)
 print(array1)
 print(array1.shape)
 print(type(array1))
 
-#Task-2 
+# Task-2
 print(array1[::-1])
 
-#Task-3
-array2 = np.arange(0,9)
-print(array2.reshape(3,3))
+# Task-3
+array2 = np.arange(0, 9)
+print(array2.reshape(3, 3))
 
-#Task-4
+# Task-4
 array3 = np.identity(5)
 print(array3)
 
-#Task-5
-array4 = np.random.randint(1,10,10) 
+# Task-5
+array4 = np.random.randint(1, 10, 10)
 print(array4)
 
-#Level-2
+# Level-2
 
-#Task-6
-array5 = np.zeros((8,8))
-array5[::2, 1::2] = 1  
-array5[1::2, ::2] = 1 
+# Task-6
+array5 = np.zeros((8, 8))
+array5[::2, 1::2] = 1
+array5[1::2, ::2] = 1
 print(array5)
 
-#Task-7
-array6 = np.ones((5,5))
+# Task-7
+array6 = np.ones((5, 5))
 array6[1:-1, 1:-1] = 0
 print(array6)
 
-#Task-8
-array7 = np.arange(1,37).reshape(6,6)
+# Task-8
+array7 = np.arange(1, 37).reshape(6, 6)
 print(array7[2:4, 2:4])
 
-#Level-3
+# Level-3
 
-#Task-9
-array8 = np.random.randint(1,101,50)
+# Task-9
+array8 = np.random.randint(1, 101, 50)
 fil_array8 = array8[array8 > 50]
 print(fil_array8)
 
-#Task-10
-array9 = np.random.randint(1,100,30)
+# Task-10
+array9 = np.random.randint(1, 100, 30)
 array9[array9 % 3 == 0] = -1
 print(array9)
 
-#Task-11
-A = np.random.randint(1,100,10)
-B = np.random.randint(1,100,10)
+# Task-11
+A = np.random.randint(1, 100, 10)
+B = np.random.randint(1, 100, 10)
 c = A > B
 print(c)
 
-#Level-4 
+# Level-4
 
-#Task-12
-array10 = np.random.randint(1,100,(4,4))
+# Task-12
+array10 = np.random.randint(1, 100, (4, 4))
 print(np.min(array10))
 print(np.sum(array10, axis=1))
 
-#Task-13
+# Task-13
 array11 = np.random.rand(20)
 print(array11.mean())
 print(array11.std())
 
-#Task-14
-array12 = np.array([[2,3,4],[5,6,7]])
+# Task-14
+array12 = np.array([[2, 3, 4], [5, 6, 7]])
 print(np.transpose(array12))
 
-#Task-15
-array13 = np.arange(1,10)
+# Task-15
+array13 = np.arange(1, 10)
 print(array13**2)
---
+++ Day-4.py
@@ -0,0 +1,56 @@
+# Importing Requirenents
+import pandas as pd
+
+# Loading The Dataset
+data = {
+    "Player": [
+        "Virat",
+        "Rohit",
+        "Rahul",
+        "Virat",
+        "Rohit",
+        "Rahul",
+        "Hardik",
+        "Hardik",
+    ],
+    "Match_Type": ["T20", "T20", "ODI", "ODI", "T20", "ODI", "T20", "ODI"],
+    "Runs": [82, 45, 110, 55, 12, 28, 64, None],  # Ek value missing hai!
+    "Balls": [53, 28, 95, 40, 9, 32, 35, 15],
+}
+df = pd.DataFrame(data)
+# Gathering Information From The Data & Of THe Data
+print(df.describe())
+print(df.info())
+# Performing Functions On The Data
+# Task 1: Handle the missing value in 'Runs' for Hardik by filling it with 0.
+df["Runs"] = df["Runs"].fillna(0)
+# Task 2: Filter the data to create a new DataFrame that contains only 'T20' matches.
+data = df[df["Match_Type"] == "T20"]
+print(data)
+# Task 3: find the total 'Runs' scored by each individual 'Player'.
+print(df.groupby("Player")["Runs"].sum().sort_values(ascending=False))
+# Task 4: Calculate the Strike Rate for every row and save it in a new column named 'Strike_Rate'.
+df["Strike_Rate"] = (df["Runs"] / df["Balls"]) * 100
+print(df)
+# Task 5: Find the missing value (NaN) generated in the 'Strike_Rate' column and fill it
+# With the Average (Mean) of that entire column.
+df["Strike_Rate"] = df["Strike_Rate"].fillna(df["Strike_Rate"].mean())
+
+
+# Determining The Batting Style
+def batting_style(strike_rate):
+    # Condition
+    if strike_rate > 130:
+        return "Aggressive"
+    else:
+        return "Anchor"
+
+
+# Assigning The Function And Craeting Another Column
+df["Batter_Style"] = df["Strike_Rate"].apply(batting_style)
+
+# Printing Cross Tabulations Table
+print(pd.crosstab(df["Player"], df["Match_Type"]))
+
+# Printing The Final Output
+print(df)
