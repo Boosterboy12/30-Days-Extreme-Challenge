5fd6408703ca336e1bb998ec2aad940697b835ab
Vihaan Singh <boosterboy2030@gmail.com>
2026-06-05 12:57:14 +0530

    modified

--- Day-11.py
+++ Day-11.py
@@ -5,10 +5,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-
-
-
-
-
-
 # Task-1: Create a Decision Tree model that predicts whether a student will pass or fail based on the number of hours studied.
+
+# ========================================================================================= #
+#                                       Solution                                            #
+# ========================================================================================= #
+# Making the data 
+
