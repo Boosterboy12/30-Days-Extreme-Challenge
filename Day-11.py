6cbb3f787fe653950d4d3e2806fe08396e5475b5
Vihaan Singh <boosterboy2030@gmail.com>
2026-06-05 12:49:45 +0530

    Created The Day-11 File

---
+++ Day-11.py
@@ -0,0 +1,14 @@
+# Importing the dependencies
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+
+
+
+
+
+
+# Task-1: Create a Decision Tree model that predicts whether a student will pass or fail based on the number of hours studied.
