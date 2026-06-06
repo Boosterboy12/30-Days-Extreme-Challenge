3553505a3ab3e6aeb9295528b120294cde5c5e44
Vihaan Singh <boosterboy2030@gmail.com>
2026-06-06 12:37:12 +0530

    modified

--- Day-12.py
+++ Day-12.py
@@ -19,15 +19,48 @@ df = pd.DataFrame(
         "Salary": [30000, 50000, 70000, 45000, 90000],
     }
 )
+
+# Creating the models & Dataframes 
 df.drop("Employee_ID", axis=1, inplace=True)
 gender_model = LabelEncoder()
 department_model = OneHotEncoder()
 age_model = StandardScaler()
 experience_model = StandardScaler()
 
+# Transforming the data
 gender_df = gender_model.fit_transform(df["Gender"])
 department_df = department_model.fit_transform(df[["Department"]])
 age_df = age_model.fit_transform(df[["Age"]])
 experience_df = experience_model.fit_transform(df[["Experience"]])
 
+# Printing the result 
 print(gender_df, department_df, age_df, experience_df)
+
+# Task-2: A school wants to predict whether a student will pass or fail based on the available information.
+
+# ========================================================================================= #
+#                                         Solution                                          #
+# ========================================================================================= #
+
+# Creating the dataset for the second task
+df_challenge_2 = pd.DataFrame({
+    "Gender": ["Male", "Female", "Male", "Female", "Male"],
+    "Study_Hours": [2, 5, 4, 1, 6],
+    "Attendance": [60, 85, 75, 50, 90],
+    "City": ["Delhi", "Mumbai", "Chennai", "Delhi", "Mumbai"],
+    "Result": ["Fail", "Pass", "Pass", "Fail", "Pass"]
+})
+
+# Creating the models 
+gender_challenge_2_model = LabelEncoder()
+hours_model = StandardScaler()
+attendance_model = StandardScaler()
+city_model = OneHotEncoder()
+
+# Transforming the data
+gender_challenge_2_df = gender_challenge_2_model.fit_transform(df_challenge_2["Gender"])
+hours_df = hours_model.fit_transform(df_challenge_2[["Study_Hours"]])
+attendance_df = attendance_model.fit_transform(df_challenge_2[["Attendance"]])
+city_df = city_model.fit_transform(df_challenge_2[["City"]])
+
+print(gender_challenge_2_df,hours_df,attendance_df,city_df)
