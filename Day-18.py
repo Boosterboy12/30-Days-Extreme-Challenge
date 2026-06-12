9306b95fe2b33ffa27e71a3c7333d912a30022c5
Vihaan Singh <boosterboy2030@gmail.com>
2026-06-12 09:51:57 +0530

    Completred Day-18 Tasks

--- Day-18.py
+++ Day-18.py
@@ -1,5 +1,5 @@
 # Importing The Dependencies
-import numpy as np 
+import numpy as np
 import torch
 
 # ========================================================================================================================== #
@@ -8,9 +8,9 @@ import torch
 # Task 1: Dimension Clash -> Create a random matrix of shape 3 * 2 and another of shape 2 * 4. Combine them mathematically to output a single matrix of shape 3 * 4.
 
 # --- Solution --- #
-random_1 = torch.rand(3,2)
-random_2 = torch.rand(2,4)
-mat = torch.matmul(random_1,random_2)
+random_1 = torch.rand(3, 2)
+random_2 = torch.rand(2, 4)
+mat = torch.matmul(random_1, random_2)
 print(mat)
 
 # ========================================================================================================================== #
@@ -35,4 +35,10 @@ else:
 # Task 3: The Shape Shifter -> Create a matrix with 2 rows and 3 columns containing numbers from 1 to 6. Change its structure completely to turn it into a single vertical column of shape 6 * 1.
 
 # --- Solution --- #
-torch.arange(1,7).shape()
+matrix = torch.tensor([[1, 2, 3], [4, 5, 6]])
+reshaped_matrix = matrix.reshape(6, 1)
+print(reshaped_matrix)
+
+# ========================================================================================================================== #
+#                                                          0 END 1                                                           #
+# ========================================================================================================================== #
