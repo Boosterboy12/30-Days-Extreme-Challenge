1711ed2096c09e00d5a03dfc0350fa4b3732318b
Vihaan Singh <boosterboy2030@gmail.com>
2026-06-13 15:13:48 +0530

    Initial Clean Commit

--- Day-19.py
+++ Day-19.py
@@ -2,6 +2,7 @@
 import os
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
 import torch
 import torch.nn as nn
 
@@ -10,6 +11,7 @@ import torch.nn as nn
 
 
 class ANN(nn.Module):
+
     # Constructor
     def __init__(self):
 
@@ -19,12 +21,26 @@ class ANN(nn.Module):
         # Input Layer -> Hidden Layer
         self.layer1 = nn.Linear(2, 3)
 
-        # Hidden Layer -> Output Layer
-        self.layer2 = nn.Linear(3, 2)
+        # Hidden Layer -> Hidden Layer
+        self.layer2 = nn.Linear(3, 4)
 
+        # Hidden Layer -> Hidden Layer
+        self.layer3 = nn.Linear(4, 8)
+
+        # Hidden Layer -> Output Layer
+        self.layer4 = nn.Linear(8, 2)
+
+    # Forward Pass
     def forward(self, x):
+
         x = self.layer1(x)
+
         x = self.layer2(x)
+
+        x = self.layer3(x)
+
+        x = self.layer4(x)
+
         return x
 
 
@@ -39,6 +55,7 @@ sample_data = torch.tensor([25.0, 180.0])
 
 # Predicting
 prediction = model(sample_data)
+
 print(prediction)
 
 # ========================================================================================================================== #
